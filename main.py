610b8483fd3f15d6308418c7e9b80bae2813afbb
Blaze <111865477+geGDVS@users.noreply.github.com>
2022-11-06 17:56:39 +0800

    Add files via upload
    考虑了儒略历至格里高利历的转变
    如果没改历法，应该可以算到宇宙毁灭

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-def month_day(month, year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0 and year % 10000 != 0) or (year % 40000 == 0 and year % 100000000 != 0) or year % 400000000 == 0:
-        mList = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+def late(y0, m0, d0, y2, m2, d2):
+    if y2 > y0:
+        return 1
+    elif y2 < y0:
+        return 2
+    elif m2 > m0:
+        return 1
+    elif m2 < m0:
+        return 2
+    elif d2 > d0:
+        return 1
+    elif d2 < d0:
+        return 2
     else:
-        mList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        return 0
+
+
+def month_day(day, month, year):
+    if late(year, month, day, 1582, 10, 4) == 2:
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0 and year % 10000 != 0) or (year % 40000 == 0 and year % 100000000 != 0) or year % 400000000 == 0:
+            mList = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        else:
+            mList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    else:
+        if year % 4 == 0:
+            mList = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        else:
+            mList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     return mList[month - 1]
 
 
@@ -11,7 +34,7 @@ def next_day(y, m, d, w):
         d = 15
     else:
         d += 1
-        if d > month_day(m, y):
+        if d > month_day(d, m, y):
             m += 1
             d = 1
         if m > 12:
@@ -34,7 +57,7 @@ def last_day(y, m, d, w):
             if m <= 0:
                 y -= 1
                 m = 12
-            d = month_day(m, y)
+            d = month_day(d, m, y)
         if m <= 0:
             y -= 1
             m = 12
@@ -51,30 +74,13 @@ print('Baseline:', y0, m0, d0, w0)
 y2, m2, d2 = eval(input('y2, m2, d2 = '))
 day = 0
 while True:
-    if y2 > y0:
+    if late(y0, m0, d0, y2, m2, d2) == 1:
         y0, m0, d0, w0 = next_day(y0, m0, d0, w0)
-        day += 1
-        print('Day:', day, '\t', y0, m0, d0, wList[w0])
-    elif y2 < y0:
+    elif late(y0, m0, d0, y2, m2, d2) == 2:
         y0, m0, d0, w0 = last_day(y0, m0, d0, w0)
-        day += 1
-        print('Day:', day, '\t', y0, m0, d0, wList[w0])
-    elif m2 > m0:
-        y0, m0, d0, w0 = next_day(y0, m0, d0, w0)
-        day += 1
-        print('Day:', day, '\t', y0, m0, d0, wList[w0])
-    elif m2 < m0:
-        y0, m0, d0, w0 = last_day(y0, m0, d0, w0)
-        day += 1
-        print('Day:', day, '\t', y0, m0, d0, wList[w0])
-    elif d2 > d0:
-        y0, m0, d0, w0 = next_day(y0, m0, d0, w0)
-        day += 1
-        print('Day:', day, '\t', y0, m0, d0, wList[w0])
-    elif d2 < d0:
-        y0, m0, d0, w0 = last_day(y0, m0, d0, w0)
-        day += 1
-        print('Day:', day, '\t', y0, m0, d0, wList[w0])
     else:
         print(day, 'days.')
         break
+    day += 1
+    print('Day:', day, '\t', y0, m0, d0, wList[w0])
+
